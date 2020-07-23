f8d4bca689183b62fd430f277ea4b5efda298a1d
nekosan5715 <nekosan5715@gmail.com>
2020-07-23 21:30:19 +0900

    テストコミット

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <>
+      This is a template
+    </>
+  )
 }
 
-export default App;
+export default App
