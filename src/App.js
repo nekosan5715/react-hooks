af0df1ddfe7437584002d271f9fe82a42ee3bf6f
nekosan5715 <nekosan5715@gmail.com>
2020-07-25 18:49:56 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(0)
+  const duble = () => setCount(count * 2)
+  const thure = () => setCount(previousCount => {
+    if (previousCount % 3 === 0) {
+    return previousCount / 3
+    } else {
+    return previousCount
+    }
+  })
+  
   return (
     <>
-      This is a template
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div>
+        <button onClick={duble}>x2</button>
+      </div>
+      <div>
+        <button onClick={thure}>3の倍数</button>
+      </div>
     </>
+    
   )
 }
 
